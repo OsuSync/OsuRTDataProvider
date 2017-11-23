6500cdf29f145452e6d38450d72fad24c85cf295
KedamaOvO <moe@mao-yu.net>
2017-11-23 23:49:44 +0800

    修复路径

--- BeatmapInfo/BeatmapSet.cs
+++ BeatmapInfo/BeatmapSet.cs
@@ -7,13 +7,31 @@ namespace MemoryReader.BeatmapInfo
 {
     public class BeatmapSet
     {
-        public int BeatmapSetID { get; private set; }
+        private int m_beatmap_id=-1;
+        public int BeatmapSetID {
+            get
+            {
+                if (m_beatmap_id > 0) return m_beatmap_id;
+                if (Setting.EnableDirectoryImprecisionSearch)
+                {
+                    var dir_list=ImprecisionSearchSongs(new DirectoryInfo(Setting.SongsPath));
+                    string name = dir_list[0].Name;
+                    int len=name.IndexOf(' ');
+                    string id = name.Substring(0,len);
+
+                    if (int.TryParse(id, out m_beatmap_id))
+                        return m_beatmap_id;
+                }
+                return -1;
+            }
+            private set=>m_beatmap_id=value;
+        }
 
         public string DownloadLink
         {
             get
             {
-                if (BeatmapSetID != 0) return @"http://osu.ppy.sh/s/" + BeatmapSetID;
+                if (BeatmapSetID > 0) return $"http://osu.ppy.sh/s/{BeatmapSetID}";
                 return LANG_NOT_FOUND;
             }
         }
@@ -29,7 +47,7 @@ namespace MemoryReader.BeatmapInfo
 
             foreach (var c in s_replace_list)
                 builder.Replace(c, "*");
-
+            
             for (int i = 0; i < builder.Length; i++)
             {
                 if (builder[i] > 127)
@@ -40,6 +58,37 @@ namespace MemoryReader.BeatmapInfo
 
         private LinkedList<string> _paths;
 
+
+        private DirectoryInfo[] ImprecisionSearchSongs(DirectoryInfo dir_info)
+        {
+            DirectoryInfo[] dir_list; 
+            dir_list = dir_info.GetDirectories(ObscureString($"*{Artist} - {Title}*"));
+            if (dir_list.Length == 0)
+            {
+                dir_list = dir_info.GetDirectories(ObscureString($"* - {Title}*"));//inso mirror bug
+            }
+            return dir_list;
+        }
+
+        private DirectoryInfo[] SearchSongs()
+        {
+            var dir_info = new DirectoryInfo(Setting.SongsPath);
+            DirectoryInfo[] dir_list;
+
+            dir_list = dir_info.GetDirectories(ObscureString($"{BeatmapSetID} {Artist} - {Title}*"));
+            if (dir_list.Length == 0)
+            {
+                dir_list = dir_info.GetDirectories(ObscureString($"{BeatmapSetID}  - {Title}*"));//inso mirror bug
+            }
+
+            if (dir_list.Length == 0 && Setting.EnableDirectoryImprecisionSearch)
+            {
+                dir_list = ImprecisionSearchSongs(dir_info);
+            }
+
+            return dir_list;
+        }
+
         public LinkedList<string> AllLocationPath
         {
             get
@@ -49,23 +98,7 @@ namespace MemoryReader.BeatmapInfo
 
                 if (_paths != null) return _paths;
 
-                var dir_info = new System.IO.DirectoryInfo(Setting.SongsPath);
-                DirectoryInfo[] dir_list;
-
-                dir_list = dir_info.GetDirectories(ObscureString($"{BeatmapSetID} {Artist} - {Title}"));
-                if (dir_list.Length == 0)
-                {
-                    dir_list = dir_info.GetDirectories(ObscureString($"{BeatmapSetID}  - {Title}"));//inso mirror bug
-                }
-
-                if (dir_list.Length == 0 && Setting.EnableDirectoryImprecisionSearch)
-                {
-                    dir_list = dir_info.GetDirectories(ObscureString($"*{Artist} - {Title}"));
-                    if (dir_list.Length == 0)
-                    {
-                        dir_list = dir_info.GetDirectories(ObscureString($"* - {Title}"));//inso mirror bug
-                    }
-                }
+                var dir_list=SearchSongs();
 
 #if DEBUG
                 Sync.Tools.IO.CurrentIO.Write($"[MemoryReader]找到的{dir_list.Length}个文件夹路径分别为:");
