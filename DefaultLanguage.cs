fab8bd7f5fdc6223328acd8d25e1508176669e88
KedamaOvO <moe@mao-yu.net>
2017-12-01 23:23:12 +0800

    clean code

--- DefaultLanguage.cs
+++ DefaultLanguage.cs
@@ -4,9 +4,9 @@ namespace MemoryReader
 {
     public class DefaultLanguage : I18nProvider
     {
-        public static LanguageElement LANG_OSU_NOT_FOUND = "[MemoryReader]没有发现 OSU! 进程";
-        public static LanguageElement LANG_OSU_FOUND = "[MemoryReader]发现 OSU! 进程";
+        public static LanguageElement LANG_OSU_NOT_FOUND = "[MemoryReader]Not found osu!.exe process";
+        public static LanguageElement LANG_OSU_FOUND = "[MemoryReader]Found osu!.exe process";
 
-        public static LanguageElement LANG_NOT_FOUND = "[MemoryReader]未找到";
+        public static LanguageElement LANG_NOT_FOUND = "[MemoryReader]Not found";
     }
 }
--- Memory/OsuModes.cs
+++ Memory/OsuModes.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MemoryReader.Memory
 {
-    public enum OsuModes
+    enum OsuModes
     {
         Menu,
         Edit,
