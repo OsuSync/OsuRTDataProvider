43238362a6ca8e47e0e0558a7121c2c57ef48b99
KedamaOvO <moe@mao-yu.net>
2019-10-02 20:08:59 +0800

    1.5.8

--- OsuRTDataProviderPlugin.cs
+++ OsuRTDataProviderPlugin.cs
@@ -17,7 +17,7 @@ namespace OsuRTDataProvider
     {
         public const string PLUGIN_NAME = "OsuRTDataProvider";
         public const string PLUGIN_AUTHOR = "KedamaOvO";
-        public const string VERSION = "1.5.7";
+        public const string VERSION = "1.5.8";
 
         private PluginConfigurationManager m_config_manager;
 
